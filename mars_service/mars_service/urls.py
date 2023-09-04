e07468f93021cdb9fad6430b0265560af956849e
Yehenii Shevchenko <sevcenkoz825@gmail.com>
2023-09-04 22:34:10 +0300

    html

--- mars_service/mars_service/urls.py
+++ mars_service/mars_service/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.conf.urls.static import static
+import orders_app.views
 
 urlpatterns = [
+    path('orders_app/', include('orders_app.urls')),
     path('admin/', admin.site.urls),
-]
+    path('', orders_app.views.mainpage, name='mainpage'),
+    path('devices/', orders_app.views.get_devices, name='get_devices'),
+    path('devpage/', orders_app.views.devpage, name='devpage')
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- mars_service/orders_app/views.py
+++ mars_service/orders_app/views.py
@@ -1,3 +1,61 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+from orders_app.models import Device, DeviceInField
+from orders_app.forms import SearchForm
+
+
+def mainpage(request):
+    data = {
+        "title": "Welcome to My site!",
+        "data": [
+            {
+                "button_link": "admin",
+                "name": "Заявки",
+                "overview": "Работа с заявками на оборудование"
+            },
+            {
+                "button_link": "devpage",
+                "name": "Персонал",
+                "overview": "Работа с базами персонала"
+            },
+            {
+                "button_link": "devpage",
+                "name": "Финансы",
+                "overview": "Работа с базами финансов"
+            },
+            {
+                "button_link": "devpage",
+                "name": "База знаний",
+                "overview": "Работа с базами знаний"
+            },
+        ]
+    }
+    return render(request, "orders_app/mainpage.html", data)
+
+
+def get_devices(request):
+    devices = DeviceInField.objects.all()
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+
+        if form.is_valid():
+            search_res = []
+            data_for_search = form.data['data_for_search']
+
+            if data_for_search.isdigit():
+                search_res = list(DeviceInField.objects.filter(analyzer_id=int(data_for_search)))
+            search_res = set(list(DeviceInField.objects.filter(customer__customer_name__contains=data_for_search)) + \
+                             list(DeviceInField.objects.filter(analyzer__manufacturer__contains=data_for_search)) + \
+                             list(DeviceInField.objects.filter(analyzer__model__contains=data_for_search)) + \
+                             list(DeviceInField.objects.filter(owner_status__contains=data_for_search)) + search_res)
+
+            return render(request, "orders_app/table_part.html", {"devices": search_res, "form": form})
+
+    return render(request, "orders_app/table_part.html", {"devices": devices})
+
+
+def devpage(request):
+    return render(request, "orders_app/devpage.html", {"title": "Oops!"})
